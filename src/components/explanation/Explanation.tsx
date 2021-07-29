1a46405d9e2aa11b0cc7d3ac5199adbda896171b
Paulo Viana <paulo9mv@yahoo.com.br>
2021-07-29 05:20:44 -0400

    feat: add more colors

--- src/components/explanation/Explanation.tsx
+++ src/components/explanation/Explanation.tsx
@@ -1,28 +1,44 @@
 import React, { FC } from "react";
-import { Typography, Box } from "@material-ui/core";
+import { Typography, Box, makeStyles } from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+
+const useStyles = makeStyles(() => ({
+  bold: {
+    fontWeight: "bold",
+  },
+}));
 
 const Explanation: FC = () => {
+  const classes = useStyles();
+
   return (
-    <Box marginTop={4}>
-      <Typography variant="h2">How is it done?</Typography>
-      <Typography>First, we sum all the elements of the array</Typography>
-      <Typography>Secondly, we start an paralel sum starting at 0</Typography>
-      <Typography>
-        Now, starting from the beginning, we iterate each element of the array
-      </Typography>
-      <Typography>
-        The element is subtracted from the total sum and the result is checked
-        if equal to paralel sum
-      </Typography>
-      <Typography>
-        If they are equal, we did it! The current position is the equilibrium
-        index.
-      </Typography>
-      <Typography>
-        Otherwise, we add the element to the paralel sum and iterate to the next
-        element, until an index is found. The total sum will decrease in each
-        iteration. If the array is over, there's no equilibrium index.
-      </Typography>
+    <Box marginTop={4} marginLeft={1} marginRight={1}>
+      <Box marginBottom={2}>
+        <Typography variant="h2">How is it done?</Typography>
+      </Box>
+      <Alert icon={false} severity="info">
+        <Typography>First, we sum all the elements of the array</Typography>
+        <Typography>Secondly, we start an paralel sum starting at 0</Typography>
+        <Typography>
+          Now, starting from the beginning, we iterate each element of the array
+        </Typography>
+        <Typography>
+          The element is subtracted from the total sum and the result is checked
+          if equal to paralel sum
+        </Typography>
+        <Typography className={classes.bold}>
+          If they are equal, we did it! The current position is the equilibrium
+          index.
+        </Typography>
+        <Typography>
+          Otherwise, we add the element to the paralel sum and iterate to the
+          next element, until an index is found. The total sum will decrease in
+          each iteration.
+        </Typography>
+        <Typography className={classes.bold}>
+          If the array is over, there's no equilibrium index.
+        </Typography>
+      </Alert>
     </Box>
   );
 };
