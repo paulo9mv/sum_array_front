0e5460494740dcf3c4f06b9e5fe9d592b5a32ac9
Paulo Viana <paulo9mv@yahoo.com.br>
2021-07-29 04:58:39 -0400

    fix: add correct url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://127.0.0.1:5000/equilibrium";
+const URL = "https://sumarray.herokuapp.com/equilibrium";
 
 interface GetApiPayload {
   arr: Array<number>;
