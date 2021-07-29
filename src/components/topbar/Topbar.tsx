9c50c9dc910b7f76a8da6c533a9bacfe5e5ce0e5
Paulo Viana <paulo9mv@yahoo.com.br>
2021-07-29 01:25:15 -0400

    refactor: adjust topbar styles

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -1,9 +1,28 @@
 import React, { FC } from "react";
 
-import "./TopBar.css";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  topbar: {
+    position: "fixed",
+    top: 0,
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    height: 40,
+    backgroundColor: "antiquewhite",
+    border: "1px solid rgba(0, 0, 0, 1)",
+    fontWeight: "bold",
+    padding: "0px 20px",
+    boxSizing: "border-box",
+  },
+}));
 
 const TopBar: FC = () => {
-  return <div className="topbar">Array Equilibrium Algorithm</div>;
+  const classes = useStyles();
+
+  return <div className={classes.topbar}>Array Equilibrium Algorithm</div>;
 };
 
 export default TopBar;
